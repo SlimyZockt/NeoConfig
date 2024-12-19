f2d74f9a254915e7645085e26e376dd99ed7daf3
slimyzockt77 <slimyzockt77@gmail.com>
2024-12-20 00:17:59 +0100

    Hypr!!

--- init.lua
+++ init.lua
@@ -651,6 +651,7 @@ require('lazy').setup({
         templ = {},
         markdown_oxide = {},
         eslint = {},
+        hyprls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
