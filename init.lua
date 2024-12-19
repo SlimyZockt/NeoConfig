bdc8c3cc9ca6eb8b9cf0971fdb3bf854c4a9dcbe
slimyzockt77 <slimyzockt77@gmail.com>
2024-12-19 23:37:32 +0100

    Nix !

--- init.lua
+++ init.lua
@@ -660,6 +660,7 @@ require('lazy').setup({
         ts_ls = {},
         dockerls = {},
         docker_compose_language_service = {},
+        nixd = {},
         --
         lua_ls = {
           -- cmd = {...},
