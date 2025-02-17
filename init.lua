edd2a9df7bcbdbbde6b5d8503553fea58d7b7a8e
slimyzockt77 <slimyzockt77@gmail.com>
2025-02-17 13:12:41 +0100

    ignore more dotfiles

--- init.lua
+++ init.lua
@@ -399,11 +399,33 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+
+          vimgrep_arguments = {
+            'rg',
+            '--follow', -- Follow symbolic links
+            '--hidden', -- Search for hidden files
+            '--no-heading', -- Don't group matches by each file
+            '--with-filename', -- Print the file path with the matched lines
+            '--line-number', -- Show line numbers
+            '--column', -- Show column numbers
+            '--smart-case', -- Smart case search
+
+            -- Exclude some patterns from search
+            '--glob=!**/.git/*',
+            '--glob=!**/.idea/*',
+            '--glob=!**/.vscode/*',
+            '--glob=!**/.godot/*',
+            '--glob=!*.import',
+            '--glob=!**/build/*',
+            '--glob=!**/dist/*',
+            '--glob=!**/yarn.lock',
+            '--glob=!**/package-lock.json',
+          },
+        },
         pickers = {
           find_files = {
             hidden = true,
@@ -417,6 +439,8 @@ require('lazy').setup({
             '--hidden',
             '--glob=!**/.git/*',
             '--glob=!**/.idea/*',
+            '--glob=!**/.godot/*',
+            '--glob=!*.import',
             '--glob=!**/.vscode/*',
             '--glob=!**/build/*',
             '--glob=!**/dist/*',
